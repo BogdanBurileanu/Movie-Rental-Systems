05e8f3d8d638c57f1346240cd8e54f3647a0c4b7
BogdanBurileanu <73855592+BogdanBurileanu@users.noreply.github.com>
2021-11-21 21:19:43 +0200

    Am creat paginile pentru noile componente

--- MovieRental/src/app/app-routing.module.ts
+++ MovieRental/src/app/app-routing.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-
+import { HomeComponent } from './home/home.component';
+import { MoviePageComponent } from './movie-page/movie-page.component';
+import { AdminComponent } from './admin/admin.component';
 const routes: Routes = [
   {
     path: 'login',component:LogInComponent
   },
   {
     path: 'signup',component:SignUpComponent
+  },
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'moviepage',component:MoviePageComponent
+  },
+  {
+    path:'admincomponent',component:AdminComponent
   },
 ];
 
--- MovieRental/src/app/app.module.ts
+++ MovieRental/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { MoviePageComponent } from './movie-page/movie-page.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
     LogInComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent,
+    MoviePageComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
