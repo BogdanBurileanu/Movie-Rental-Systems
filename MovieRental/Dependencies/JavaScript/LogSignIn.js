87cbad879f2f4ac3e1ef992bb4c5112674f749bc
BogdanBurileanu <73855592+BogdanBurileanu@users.noreply.github.com>
2021-12-07 17:44:42 +0200

    Login

---
+++ MovieRental/Dependencies/JavaScript/LogSignIn.js
@@ -0,0 +1,25 @@
+function BckLogin()
+{
+    document.body.style.backgroundImage="url(../Dependencies/ImageAssets/slideshow1.png)";
+    
+}
+function BckSignin()
+{
+    document.body.style.backgroundImage="url(../Dependencies/ImageAssets/slideshow2.png)";
+    
+}
+
+function change() 
+{
+    document.getElementById('b1').innerText = "To access all the features,please sign in."
+}
+
+function change2() 
+{
+    document.getElementById('b1').innerHTML = "To access all the features,please log in."
+}
+
+function HomePg()
+{
+    window.location.href="../main.html"
+}
