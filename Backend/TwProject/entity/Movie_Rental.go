73a9efff4eaca15974d4bc3a099552f21a8460ce
andreibugariu <72136299+andreibugariu@users.noreply.github.com>
2021-12-23 10:36:59 +0200

    Delete Backend/TwProject directory

--- Backend/TwProject/entity/Movie_Rental.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-type Movie_Rental struct {
-	ID       string `gorm:"type:uuid;default:uuid_generate_v4();primary_key;column:id"`
-	RentalID string `gorm:"type:uuid;column:rental_id;not null" validate:"required"`
-	MovieID  string `gorm:"type:uuid;column:movie_id;not null" validate:"required"`
-}
