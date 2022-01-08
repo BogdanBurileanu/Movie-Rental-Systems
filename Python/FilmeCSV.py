b24b2339eec49eb02d6df4d9031572cff77b75aa
BogdanBurileanu <73855592+BogdanBurileanu@users.noreply.github.com>
2022-01-08 11:47:27 +0200

    Update FilmeCSV.py
    Implementat link trailer yt

--- Python/FilmeCSV.py
+++ Python/FilmeCSV.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[26]:
+# In[8]:
 
 
 import csv
 import imdb
 import numpy as np
+import urllib.request
+import re
+def url_search(name):
+    search_keyword=name + "trailer"
+    search_keyword=search_keyword.replace(" ","+")
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    return("https://www.youtube.com/watch?v=" + video_ids[0])
 IM=imdb.IMDb()
 x=0
 n=int(input("n="))
@@ -20,7 +28,7 @@ while(x!=n):
     vec=np.array(lista)
 with open('Filme.csv','w',newline='')as file:
     writer=csv.writer(file)
-    writer.writerow(["Nume film","Gen film","Director","Nota IMDB","Descriere"])
+    writer.writerow(["Nume film","Gen film","Director","Nota IMDB","Descriere","Trailer"])
     for i in vec:
         nume=IM.get_movie(i)
         for director in nume['directors']:
@@ -29,8 +37,9 @@ with open('Filme.csv','w',newline='')as file:
         genre=nume.data['genres']
         rating=nume.data['rating']
         desc=nume.data['plot outline']
+        
     
-        writer.writerow([nume,genre,director,rating,desc])
+        writer.writerow([nume,genre,director,rating,desc,url_search(nume_film[0]['title'])])
     
 
 
